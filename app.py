a28f527bbd78e01d49cea651bdf6f07468d22d2c
liyouzhi <lyz@buaa.us>
2017-03-15 15:44:37 +0800

    edited app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 # -*- coding:utf-8 -*-
+import time
+import uuid
+import os
+import magic
+import json
 
 from flask import Flask, render_template
 from flask import request
 from flask import send_from_directory, make_response
 from werkzeug.utils import secure_filename
-
-import time
-import uuid
-import os
 
 UPLOAD_FOLDER = '/Users/liyouzhi/dev/python/zimg-python/image_storage'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -26,27 +27,53 @@ def allowed_file(filename):
 def home():
     return render_template('index.html')
 
-@app.route('/<image_id>', methods=['GET'])
+
+# RESTful api
+# http status code
+# multipart/form
+# curl
+# optimize id generator
+# timestamp
+
+@app.route('/images', methods=['POST'])
+def post_image():
+    data = request.get_data()
+    id = next_id()
+    print('id:', id)
+    with open(os.path.join(app.config['UPLOAD_FOLDER'], id), 'wb') as f:
+        f.write(data)
+    ret = { 'key': id }
+    return json.dumps(ret)
+
+@app.route('/images/multipart', methods=['POST'])
+def multipart_post_image():
+    file = request.files['file']
+    if not file:
+        response.status_code = 400
+        return 'upload file not found'
+
+    if not allowed_file(file.filename):
+        response.status_code = 400
+        return 'upload file illegal'
+
+    id = next_id()
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], id))
+    ret = { 'key': id }
+    return json.dumps(ret)
+
+@app.route('/images/<image_id>', methods=['GET'])
 def get_image(image_id):
-    if len(image_id) == 64:
-        return '<img src="%s" />' % os.path.join(app.config['UPLOAD_FOLDER'], image_id)
+    with open(os.path.join(app.config['UPLOAD_FOLDER'], image_id), 'rb') as f:
+        data = f.read()
+    mtype = magic.from_buffer(data, mime=True)
+    response = make_response(data)
+    response.headers['Content-Type'] = mtype
+    return response
 
 @app.route('/download/<image_id>')
 def download_image(image_id):
     return send_from_directory(app.config['UPLOAD_FOLDER'], image_id)
 
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    file = request.files['file']
-    if file and allowed_file(file.filename):
-        id = next_id()
-        #file.filename = secure_filename(id)
-        file.filename = id + '.' + file.filename.rsplit('.', 1)[1]
-        #print('filename:', file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        return '<h3>key: %s<h3>' % id
-
 if __name__ == '__main__':
     app.run(debug=True)
-
 

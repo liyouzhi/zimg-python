a8a79ec31c536c9b671c9f2b5e43f35fb5e65421
liyouzhi <lyz@buaa.us>
2017-03-22 18:48:13 +0800

    Add expiration time in LRU

--- LRUCache.py
+++ LRUCache.py
@@ -1,30 +1,41 @@
 # -*- coding:utf-8 -*-
 from collections import OrderedDict
+from time
 
 class LRUCache(object):
 
-def __init__(self, capacity):
-    self.__capacity = capacity
-    self.__cache = OrderedDict()
+    def __init__(self, capacity=1024, TTL=24*60*60):
+        self.capacity = capacity
+        self.TTL = TTL
 
-def set(self, key, value):
-    try:
-        self.__cache.pop(key)
-    except KeyError:
-        if len(self.cache) >= self.__capacity:
-            self.__cache.popitem(last=False)
-    self.__cache[key] = value
+        self.__cache = OrderedDict()
+        self.__expiration_time = {}
 
-def get(self, key):
-    try:
-        value = self.__cache.pop(key)
+    def set(self, key, value):
+        try:
+            self.__delete(key)
+        except KeyError:
+            if len(self.cache) >= self.__capacity:
+                k, v = self.__cache.popitem(last=False)
+                del self.__expiration_time[k]
         self.__cache[key] = value
-        return value
-    except KeyError:
-        return -1
+        t = int(time.time())
+        self.__expiration_time[key] = t + TTL 
 
-def clear(self):
-    self.__cache.clear()
+    def get(self, key):
+        try:
+            value = self.__cache.pop(key)
+            self.__cache[key] = value
+            return value
+        except KeyError:
+            return -1 
 
+    def clear(self):
+        self.__cache.clear()
+        self.__expiration_time.clear()
+
+    def __delete(self, key):
+        del self.__cache[key]
+        del self.__expiration_time[key]
 
 
